bd2e9e0381aa514aab0f049ba24c75bd038ae9e9
chemidong <chemidong@gmail.com>
2017-10-12 21:45:12 +0900

    Pintos Project 1

---
+++ pintos_#1/src/tests/threads/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+
+
+void
+test_hello (void)
+{
+  printf ("########## 20130416 #############\n");
+  printf ("~~~~~~HELLO PINTOS WORLD~~~~~~~~\n");
+  return 0;
+}
+
+
